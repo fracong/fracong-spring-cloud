66c6db4a9425f4c864907e0ffc51f20fdc0d294d
fangcong <fangcong@ctvit.com.cn>
2019-05-28 10:50:38 +0800

    升级Springboot的版本为2.1版本

--- fracong-manage/src/main/java/com/fracong/manage/ManageApplication.java
+++ fracong-manage/src/main/java/com/fracong/manage/ManageApplication.java
@@ -2,12 +2,8 @@ package com.fracong.manage;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.cloud.netflix.feign.EnableFeignClients;
 
 @SpringBootApplication
-@EnableDiscoveryClient
-@EnableFeignClients(basePackages = "com.fracong.*.controller")
 public class ManageApplication {
 
 	public static void main(String[] args) {
