0e9a5ae3228d95c113917bf8c56dc6c09de9bd0e
fangcong <fangcong@ctvit.com.cn>
2019-05-30 15:36:32 +0800

    修改定时任务时间

--- fracong-manage/src/main/java/com/fracong/manage/scheduling/TestScheduling.java
+++ fracong-manage/src/main/java/com/fracong/manage/scheduling/TestScheduling.java
@@ -11,7 +11,7 @@ import com.fracong.util.date.DateUtil;
 @EnableScheduling
 public class TestScheduling {
 	
-	@Scheduled(fixedDelay=3000)//300s执行1次此方法;
+	@Scheduled(fixedDelay=300000)//300s执行1次此方法;
     public void sendType1() throws Exception{
 		System.err.println("sendType1---->"+DateUtil.getNowDateStringByType(DateType.TYPE_1));
 	}
@@ -21,12 +21,12 @@ public class TestScheduling {
 		System.err.println("sendType2---->"+DateUtil.getNowDateStringByType(DateType.TYPE_1));
 	}
 	
-	@Scheduled(fixedRate=3000)//说明：上一次执行在预定的时间内，会按改时间执行，如果超过执行时间，则上一次执行完成，立即执行
+	@Scheduled(fixedRate=300000)//说明：上一次执行在预定的时间内，会按改时间执行，如果超过执行时间，则上一次执行完成，立即执行
     public void sendType23() throws Exception{
 		System.err.println("sendType3---->"+DateUtil.getNowDateStringByType(DateType.TYPE_1));
 	}
 	
-	@Scheduled(cron="*/30  * * * * ?")//表达式执行
+	@Scheduled(cron="* */30 * * * ?")//表达式执行
     public void sendType4() throws Exception{
 		System.err.println("sendType4---->"+DateUtil.getNowDateStringByType(DateType.TYPE_1));
 	}
