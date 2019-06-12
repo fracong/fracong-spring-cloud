3bd1438bd9ce805170de082ac1327d1b8ca0b7dd
fracong <fracong@sina.cn>
2019-06-12 12:55:45 +0800

    添加SpringCloud Gateway的基本使用(无过滤器)： 1.当配置为【Path=/fracong-service/**】、【uri: http://localhost:8889】时，访问【http://[gateway_server_ip:gateway_server_post]/fracong-service/***】的时候，会自动转到【http://localhost:8889/fracong-service/***】上； 2.当配置为【Path=/fracong-manage/**】、【uri: lb://fracong-manage】时(前提：配置eureka,而lb://为约定写成)，在访问【http://[gateway_server_ip:gateway_server_post]/fracong-manage/***】的时候，会先去eureka中获取配置信息，然后根据匹配自动转到【http://localhost:8889/fracong-manage/***】上; 3.fracong-cloud-gateway的pom.xml文件加入【spring-cloud-starter-gateway】之后，springboot2.x版本不需要添加【spring-cloud-starter-web】，因为在【spring-cloud-starter-gateway】中已经集成了【spring-cloud-starter-web】； 4.在使用【spring-cloud-starter-gateway】后，server.servlet.content-path无效

---
+++ fracong-cloud-gateway/src/main/java/com/fracong/springcloud/gateway/SpringCloudGatewayApplication.java
@@ -0,0 +1,14 @@
+package com.fracong.springcloud.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+public class SpringCloudGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringCloudGatewayApplication.class, args);
+	}
+}
