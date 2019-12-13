17bf75fa5cceeebb020a7f655231d733fa2d62e5
fracong <fracong@sina.com>
2019-12-13 21:31:58 +0800

    add insert content page

--- fracong-blog-service/src/main/java/com/fracong/blog/func/content/controller/ContentController.java
+++ fracong-blog-service/src/main/java/com/fracong/blog/func/content/controller/ContentController.java
@@ -1,8 +1,16 @@
 package com.fracong.blog.func.content.controller;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.InputStreamReader;
+
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -22,5 +30,26 @@ public class ContentController {
 		BlogContent content = contentService.getContentById(id);
 		String returnStr = JSON.toJSONStringWithDateFormat(content, "yyyy-MM-dd HH:mm:ss", SerializerFeature.WriteMapNullValue);
 		return returnStr;
+	}
+	
+	@PostMapping("/insertContent/{key}")
+	public String insertContent(@PathVariable(name="key") String key,
+			@RequestBody BlogContent content) throws Exception{
+		String retrunStr = "false";
+		File file = new File("/opt/key.txt");
+		//File file = new File("G:\\key.txt");
+		InputStreamReader read = new InputStreamReader(new FileInputStream(file),"UTF-8");
+		BufferedReader br = new BufferedReader(read);
+		String text = "";
+		String textLine = null;
+		while((textLine =br.readLine()) != null){
+			text += textLine;
+		}
+		read.close();
+		if(StringUtils.isNotBlank(text) && text.equals(key)) {
+			contentService.insertContent(content);
+			retrunStr = "ok";
+		}
+		return retrunStr;
 	}
 }
--- fracong-blog-service/src/main/java/com/fracong/blog/func/content/service/ContentService.java
+++ fracong-blog-service/src/main/java/com/fracong/blog/func/content/service/ContentService.java
@@ -1,5 +1,7 @@
 package com.fracong.blog.func.content.service;
 
+import java.util.Date;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -13,7 +15,30 @@ public class ContentService {
 
 	public BlogContent getContentById(Long id) {
 		BlogContent blogContent = blogContentDao.selectByPrimaryKey(id);
+		BlogContent newBlogContent = new BlogContent();
+		newBlogContent.setContentId(id);
+		newBlogContent.setViewNum(blogContent.getViewNum()+1);
+		blogContentDao.updateByPrimaryKeySelective(newBlogContent);
 		return blogContent;
+	}
+
+	public void insertContent(BlogContent content) {
+		content.setContentId(1L);
+		content.setUpdateTime(new Date());
+		content.setCreateTime(new Date());
+		String[] split = content.getContent().split("\n|\r");
+		String contentNew = "";
+		for (String p : split) {
+			if(!p.startsWith("<p>")){
+				p = "<p>"+p;
+			}
+			if(!p.endsWith("</p>")){
+				p +="</p>";
+			}
+			contentNew +=p;
+		}
+		content.setContent(contentNew);
+		blogContentDao.updateByPrimaryKeySelective(content);
 	}
 
 }
