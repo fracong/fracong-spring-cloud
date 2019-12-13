a848990d240f18ec83644e7118ec7e1acb887560
fracong <237093889@qq.com>
2019-12-13 11:50:26 +0800

    返回数据时间格式化问题，mybatis连接数据库时差问题：数据库连接配置

--- fracong-blog-service/src/main/java/com/fracong/blog/func/content/controller/ContentController.java
+++ fracong-blog-service/src/main/java/com/fracong/blog/func/content/controller/ContentController.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.serializer.SerializerFeature;
 import com.fracong.blog.func.content.entity.BlogContent;
 import com.fracong.blog.func.content.service.ContentService;
 
@@ -19,6 +20,7 @@ public class ContentController {
 	@GetMapping("/getContent/{id}")
 	public String getContent(@PathVariable(name="id") Long id){
 		BlogContent content = contentService.getContentById(id);
-		return JSON.toJSONString(content);
+		String returnStr = JSON.toJSONStringWithDateFormat(content, "yyyy-MM-dd HH:mm:ss", SerializerFeature.WriteMapNullValue);
+		return returnStr;
 	}
 }
